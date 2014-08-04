57f7fccbf8170fa6eddfcbfb18429abe72df11de
Martin Endler <pokusew@seznam.cz>
2014-08-04 15:09:13 +0200

    added support for MethodProperty in Entity (get, set, isset) renamed package changed typo in Entity

--- src/YetORM/Entity.php
+++ src/YetORM/Entity.php
@@ -14,7 +14,6 @@ namespace YetORM;
 use Nette;
 use Nette\Database\Table\ActiveRow as NActiveRow;
 
-
 abstract class Entity
 {
 
@@ -24,20 +23,17 @@ abstract class Entity
 	/** @var array */
 	private static $reflections = array();
 
-
 	/** @param  NActiveRow|Record $row */
 	function __construct($row = NULL)
 	{
 		$this->record = Record::create($row);
 	}
 
-
 	/** @return Record */
 	final function toRecord()
 	{
 		return $this->record;
 	}
-
 
 	/**
 	 * @param  string $name
@@ -50,7 +46,6 @@ abstract class Entity
 		throw new Exception\MemberAccessException("Call to undefined method $class::$name().");
 	}
 
-
 	/**
 	 * @param  string $name
 	 * @return mixed
@@ -58,15 +53,22 @@ abstract class Entity
 	function & __get($name)
 	{
 		$prop = static::getReflection()->getEntityProperty($name);
-		if ($prop instanceof Reflection\AnnotationProperty) {
+		
+		if ($prop instanceof Reflection\AnnotationProperty)
+		{
 			$value = $prop->setType($this->record->{$prop->column});
+			return $value;
+		}
+		
+		if ($prop instanceof Reflection\MethodProperty)
+		{
+			$value = $this->{'get' . ucfirst($prop->name)}();
 			return $value;
 		}
 
 		$class = get_class($this);
 		throw new Exception\MemberAccessException("Cannot read an undeclared property $class::\$$name.");
 	}
-
 
 	/**
 	 * @param  string $name
@@ -76,16 +78,24 @@ abstract class Entity
 	function __set($name, $value)
 	{
 		$prop = static::getReflection()->getEntityProperty($name);
-		if ($prop instanceof Reflection\AnnotationProperty && !$prop->readonly) {
+		
+		if ($prop instanceof Reflection\AnnotationProperty && !$prop->readonly)
+		{
 			$prop->checkType($value);
 			$this->record->{$prop->column} = $value;
-			return ;
+			return;
+		}
+		
+		if ($prop instanceof Reflection\MethodProperty && !$prop->readonly)
+		{
+			$prop->checkType($value);
+			$this->{'set' . ucfirst($prop->name)}();
+			return;
 		}
 
 		$class = get_class($this);
 		throw new Exception\MemberAccessException("Cannot write to an undeclared property $class::\$$name.");
 	}
-
 
 	/**
 	 * @param  string $name
@@ -94,13 +104,14 @@ abstract class Entity
 	function __isset($name)
 	{
 		$prop = static::getReflection()->getEntityProperty($name);
-		if ($prop instanceof Reflection\AnnotationProperty) {
+		
+		if ($prop instanceof Reflection\AnnotationProperty || $prop instanceof Reflection\MethodProperty)
+		{
 			return $this->__get($name) !== NULL;
 		}
 
 		return FALSE;
 	}
-
 
 	/**
 	 * @param  string $name
@@ -112,12 +123,12 @@ abstract class Entity
 		throw new Exception\NotSupportedException;
 	}
 
-
 	/** @return Reflection\EntityType */
 	static function getReflection()
 	{
 		$class = get_called_class();
-		if (!isset(self::$reflections[$class])) {
+		if (!isset(self::$reflections[$class]))
+		{
 			self::$reflections[$class] = new Reflection\EntityType($class);
 		}
 
